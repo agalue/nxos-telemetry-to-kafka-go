c901a7820aedf4afa2e76530304acc0bbb26642f
Alejandro Galue <agalue@opennms.org>
2021-03-28 11:06:33 -0400

    Fix code generation with protoc.

--- api/build.sh
+++ api/build.sh
@@ -2,7 +2,7 @@
 
 type protoc >/dev/null 2>&1 || { echo >&2 "protoc required but it's not installed; aborting."; exit 1; }
 
-protoc -I . sink.proto --go_out=./sink
-protoc -I . telemetry.proto --go_out=./telemetry
-protoc -I . telemetry_bis.proto --go_out=./telemetry_bis
-protoc -I . mdt_dialout.proto --go_out=plugins=grpc:./mdt_dialout
+protoc --proto_path=./ --go_out=./ sink.proto
+protoc --proto_path=./ --go_out=./ telemetry.proto
+protoc --proto_path=./ --go_out=./ telemetry_bis.proto
+protoc --proto_path=./ --go_out=plugins=grpc:./ mdt_dialout.proto
